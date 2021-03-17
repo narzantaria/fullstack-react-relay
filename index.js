4d4c0fce1c90dd01de0a8d5a24e2581e61dbd92f
Nar Zantaria <narzantaria@gmail.com>
2021-03-17 23:40:06 +0300

    first

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require('express');
-const path = require('path');
 const cors = require('cors');
 const config = require('config');
-
-// const { addHero, getHeroes } = require('./data/db');
 
 const { graphqlHTTP } = require('express-graphql');
 const { GraphQLSchema } = require('graphql');
@@ -22,8 +19,6 @@ const schema = new GraphQLSchema({
 const app = express();
 
 app.use(cors());
-// Init Middleware
-// app.use(express.json());
 
 app.use(
   "/graphql",
@@ -33,13 +28,9 @@ app.use(
   })
 );
 
-//This is for open production client, should be stored in 'dist' folder:
 app.get('/', (req, res) => {
-  // res.sendFile(path.join(__dirname + '/dist/index.html'));
   res.send("Shutruk-Nahhunte");
 });
-
-// app.use(express.static(path.join(__dirname, 'dist')));
 
 app.listen(PORT, _ => console.log(`Server started at ${PORT}...`));
 // app.listen(PORT,HOST, _ => console.log(`Server started at ${PORT}...`));
